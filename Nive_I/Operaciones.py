4561c80cbe121905b03d7fbca5e045c6f5ad113a
Anderson <ander.yovera2022mail.com>
2026-02-03 19:59:28 -0500

    Se agrego ejercicios del nivel I

---
+++ Nive_I/Operaciones.py
@@ -0,0 +1,15 @@
+numI = int(input("Escribe un numero entero: "))
+numII = float(input("Escribe un numero decimal: "))
+
+suma = numI + numII
+resta = numI - numII
+multiplicacion = numI * numII
+division = numI / numII
+potencia = numI ** numII
+
+
+print(f"La suma de ambos numero es : {suma}")
+print(f"La resta de ambos numero es : {resta}")
+print(f"La multiplicacion de ambos numero es : {multiplicacion}")
+print(f"La division de ambos numero es : {division}")
+print(f"La potencia de ambos numero es : {potencia}")
---
+++ Nive_I/imprime.py
@@ -0,0 +1,5 @@
+nombre = str(input("Escribe tu nombre: "))   #texto
+edad = int(input("Cual es tu edad: "))      #numero entero
+altura = float(input("Cual es tu altura: "))    #numero decimal
+print(f"""Hola, estimado {nombre} mucho gusto de saludarte
+      tienes {edad} años y mides {altura} metros""")                    
